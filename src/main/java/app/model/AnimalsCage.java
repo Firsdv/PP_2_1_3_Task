042865b733469b4eaa6251718473d5e6509175f8
Firsdv <firsdv8@gmail.com>
2025-03-16 13:26:33 +0300

    Исправил - теперь время одинаковое!

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -8,9 +8,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class AnimalsCage {
 
-    @Qualifier("myDog")
-
     @Autowired
+    @Qualifier("myDog")
     private Animal animal;
     private final Timer timer;
 
@@ -27,7 +26,7 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
